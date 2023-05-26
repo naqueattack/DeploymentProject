5944a7497ce3652ac829296112ff6a53c4eba6c9
Andrew Weber <naqueattack@gmail.com>
2023-05-25 21:00:53 -0500

    Troubleshooting and successful AWS upload (test notebook)

--- notebooks/api.py
+++ notebooks/api.py
@@ -3,34 +3,47 @@ from flask import Flask, jsonify, request
 # import Resource, Api and reqparser
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
-import numpy
+import numpy as np
 import pickle
 
 app = Flask(__name__)
 api = Api(app)
 
-def numFeat(X):
-    return X[num_feats]
-
 def catFeat(X):
+    cat_feats = X.dtypes[X.dtypes == 'object'].index.tolist()
     return X[cat_feats]
 
+def numFeat(X):
+    cat_feats = X.dtypes[X.dtypes == 'object'].index.tolist()
+    num_feats = X.dtypes[~X.dtypes.index.isin(cat_feats)].index.tolist()
+    return X[num_feats]
+
 def logtransform(X):
+    print(X)
     X['LoanAmount'] = np.log(X['LoanAmount'])
     X['TotalIncome'] = np.log(X['ApplicantIncome']+X['CoapplicantIncome'])
     X = X.drop(columns = ['ApplicantIncome','CoapplicantIncome'])
     return X
-    
+
 model = pickle.load( open( "C:/Users/Andrew/Documents/LHL/Deployment/DeploymentProject/notebooks/model.p", "rb" ) )
 
 class Prediction(Resource):
     def post(self):
+        # global cat_feats
+        # global num_feats
         json_data = request.get_json()
         df = pd.DataFrame(json_data.values(), index=json_data.keys()).transpose()
+        print(df)
+        
+        #change numeric variables to float
+        df[['ApplicantIncome','CoapplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History']] = df[['ApplicantIncome','CoapplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History']].astype('float')
         # getting predictions from our model.
-        res = model.predict_proba(df)
+        res = model.predict(df)
         # we cannot send numpt array as a result
         return res.tolist() 
+    
+    def get(self):
+        return("What are you doing here? Try posting")
     
 # assign endpoint
 api.add_resource(Prediction, '/prediction')
